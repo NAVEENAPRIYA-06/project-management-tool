76c22c8b3b29b4233e2c46fe5cb7cd437f38db73
NAVEENAPRIYA J <jnaveenapriya2006@gmail.com>
2025-09-22 13:49:42 +0530

    Added a user routes

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+const asyncHandler = require('express-async-handler');
+const User = require('../models/User');
+
+// @desc    Register new user
+// @route   POST /api/users
+router.post(
+  '/',
+  asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      res.status(400);
+      throw new Error('Please add all fields');
+    }
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      res.status(400);
+      throw new Error('User already exists');
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    if (user) {
+      res.status(201).json({
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+      });
+    } else {
+      res.status(400);
+      throw new Error('Invalid user data');
+    }
+  })
+);
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -4,12 +4,13 @@ const connectDB = require('./config/db');
 const app = express();
 const cors = require('cors'); // Add this line
 const port = process.env.PORT || 5000;
-
+const userRoutes = require('./routes/userRoutes');
 connectDB();
 
 app.use(cors()); // Add this line
 app.use(express.json());
 app.use('/api/tasks', require('./routes/taskRoutes'));
+app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello from the server!');
